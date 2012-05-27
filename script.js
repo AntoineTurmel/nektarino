967a445576cdd9cdcb5cddc05da52b4655ee87b0
Antoine Turmel <geekshadow@gmail.com>
2012-05-27 05:20:31 +0200

    more bits

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Nektarino JS
+var playing = false
+var nectaaudio = new Audio("http://scenemusic.unfy.org/necta64.ogg");
 
 function playradio() {
-		var nectaaudio = new Audio("http://scenemusic.unfy.org/necta64.ogg");
 		nectaaudio.load();
 		nectaaudio.play();
+    playing = true;
+    document.getElementById('pause').style.visibility='visible';
+    document.getElementById('pause').style.display='block';
+    document.getElementById('play').style.visibility='hidden';
+    document.getElementById('play').style.display='none';    
+}
+
+function pauseradio() {
+    nectaaudio.pause();
+    playing = false;
+    document.getElementById('play').style.visibility='visible';
+    document.getElementById('play').style.display='block';
+    document.getElementById('pause').style.visibility='hidden';
+    document.getElementById('pause').style.display='none';
 }
